250bac77d4f28d15c7205f0600136cf9f43364e6
meck <Johan@meck.se>
2022-01-02 13:51:10 +0100

    Style code

--- app/Main.hs
+++ app/Main.hs
@@ -1,117 +1,133 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 
-import           Lib
-import qualified ClientSecret                  as CS
-import           Images
-import           Control.Exception
-import           Data.Aeson
-import           Text.PrettyPrint.Boxes  hiding ( (<>) )
-import qualified Text.PrettyPrint.Boxes        as B
-                                                ( (<>) )
-import           Data.Time.Clock
-import           Data.Time.Calendar
-import           Data.Time.LocalTime
-import           System.Exit
-import           System.Directory
-import           System.IO.Error
-import           Options.Applicative
-import           Network.HTTP.Client.Conduit
+import qualified ClientSecret as CS
+import Control.Exception
+import Data.Aeson
+import Data.Time.Calendar
+import Data.Time.Clock
+import Data.Time.LocalTime
+import Images
+import Lib
+import Network.HTTP.Client.Conduit
+import Options.Applicative
+import System.Directory
+import System.Exit
+import System.IO.Error
+import Text.PrettyPrint.Boxes hiding ((<>))
+import qualified Text.PrettyPrint.Boxes as B
+  ( (<>),
+  )
 
 data ArgOpts = ArgOpts
-  { formatForBB :: Bool
-  , authParams:: Maybe AuthCode }
-  deriving Show
+  { formatForBB :: Bool,
+    authParams :: Maybe AuthCode
+  }
+  deriving (Show)
 
 argOpts :: Parser ArgOpts
 argOpts =
   ArgOpts
     <$> switch
-          ((long "bitbar" <> short 'b') <> help "Format the output for bitbar")
+      ((long "bitbar" <> short 'b') <> help "Format the output for bitbar")
     <*> optional
-          (strOption
-            (long "auth" <> metavar "AUTHCODE" <> help
-              "Get a new token using a auth code from the website"
-            )
+      ( strOption
+          ( long "auth" <> metavar "AUTHCODE"
+              <> help
+                "Get a new token using a auth code from the website"
           )
+      )
 
 main :: IO ()
 main = do
-  opts <- execParser $ info
-    (argOpts <**> helper)
-    (fullDesc <> header "noxstatus - get invoice status from fortnox.se")
+  opts <-
+    execParser $
+      info
+        (argOpts <**> helper)
+        (fullDesc <> header "noxstatus - get invoice status from fortnox.se")
 
   case authParams opts of
     Just ac -> newTokenFile ac
     Nothing -> do
       token <- loadTokenFile
-      cDay  <- currentDay
-      res   <- getInvoices token
-      res'  <- case res of
+      cDay <- currentDay
+      res <- getInvoices token
+      res' <- case res of
         Left (ConnectFail (HttpExceptionRequest _ (ConnectionFailure _))) ->
           if formatForBB opts
             then exitFailure
             else die "Error: Connection failed"
-        Left  e -> die $ "Error: \n" <> show e
+        Left e -> die $ "Error: \n" <> show e
         Right r -> return r
       putStr $ makeTable (formatForBB opts) cDay res'
       return ()
 
 makeTable :: Bool -> Day -> ([CustomerInvoice], [SupplierInvoice]) -> String
-makeTable forBB today (cus, sup) = if forBB && nCus + nSup < 1
-  then []
-  else barItem <> render table
- where
-  nCus      = length cus
-  nSup      = length sup
-  diffDays' = text . show . diffDays today
-  daysC     = cWithLabels
-    left
-    [ (diffDays' . cDueDate <$> cus, "Days:")
-    , (diffDays' . sDueDate <$> sup, "Days:")
-    ]
-  custC = cWithLabels
-    left
-    [(text . cName <$> cus, "Customer:"), (text . sName <$> sup, "Supplier:")]
-  balC = cWithLabels
-    right
-    [ (text . show . cBalance <$> cus, "Sum:")
-    , (text . show . sTotal <$> sup  , "Sum:")
-    ]
-  currC = cWithLabels
-    left
-    [(text . cCurrency <$> cus, ""), (text . sCurrency <$> sup, "")]
-  balC'   = hsep 1 top [balC, currC]
-  fontStr = "| font=Courier size=14"
-  linkStr = " href=http://fortnox.se"
-  formatC = if forBB
-    then cWithLabels
-      left
-      [ (text (fontStr <> linkStr) <$ cus, fontStr)
-      , (text (fontStr <> linkStr) <$ sup, fontStr)
-      ]
-    else nullBox
-  table   = hsep 2 top [daysC, custC, balC'] B.<> formatC
-  barItem = if forBB
-    then unlines
-      [ show nCus
-      <> "/"
-      <> show nSup
-      <> "| templateImage="
-      <> fnIcon
-      <> " size=16 font=Courier"
-      , "---"
-      ]
-    else unlines
-      [ "Unpaid customer invoices: " <> show nCus
-      , "Unpaid supplier invoices: " <> show nSup
-      , ""
-      ]
-  cWithLabels itemA bss = vcat left $ subCol <$> bss
-   where
-    subCol (subItems, label) =
-      if null subItems then nullBox else text label // vcat itemA subItems
+makeTable forBB today (cus, sup) =
+  if forBB && nCus + nSup < 1
+    then []
+    else barItem <> render table
+  where
+    nCus = length cus
+    nSup = length sup
+    diffDays' = text . show . diffDays today
+    daysC =
+      cWithLabels
+        left
+        [ (diffDays' . cDueDate <$> cus, "Days:"),
+          (diffDays' . sDueDate <$> sup, "Days:")
+        ]
+    custC =
+      cWithLabels
+        left
+        [(text . cName <$> cus, "Customer:"), (text . sName <$> sup, "Supplier:")]
+    balC =
+      cWithLabels
+        right
+        [ (text . show . cBalance <$> cus, "Sum:"),
+          (text . show . sTotal <$> sup, "Sum:")
+        ]
+    currC =
+      cWithLabels
+        left
+        [(text . cCurrency <$> cus, ""), (text . sCurrency <$> sup, "")]
+    balC' = hsep 1 top [balC, currC]
+    fontStr = "| font=Courier size=14"
+    linkStr = " href=http://fortnox.se"
+    formatC =
+      if forBB
+        then
+          cWithLabels
+            left
+            [ (text (fontStr <> linkStr) <$ cus, fontStr),
+              (text (fontStr <> linkStr) <$ sup, fontStr)
+            ]
+        else nullBox
+    table = hsep 2 top [daysC, custC, balC'] B.<> formatC
+    barItem =
+      if forBB
+        then
+          unlines
+            [ show nCus
+                <> "/"
+                <> show nSup
+                <> "| templateImage="
+                <> fnIcon
+                <> " size=16 font=Courier",
+              "---"
+            ]
+        else
+          unlines
+            [ "Unpaid customer invoices: " <> show nCus,
+              "Unpaid supplier invoices: " <> show nSup,
+              ""
+            ]
+    cWithLabels itemA bss = vcat left $ subCol <$> bss
+      where
+        subCol (subItems, label) =
+          if null subItems then nullBox else text label // vcat itemA subItems
 
 currentDay :: IO Day
 currentDay =
@@ -122,18 +138,19 @@ tokenPath = getXdgDirectory XdgConfig $ "noxstatus" <> "/" <> "fn_token.json"
 
 loadTokenFile :: IO FnToken
 loadTokenFile = do
-  tp      <- tokenPath
+  tp <- tokenPath
   cfgFile <-
     eitherDecodeFileStrict' tp
-      `catch` (\e -> if isDoesNotExistError e
-                then
-                  die
-                    "Error: Could not find token file, generate one with \"--auth\" argument"
-                else die $ "Error opening token file: \n" <> show e
+      `catch` ( \e ->
+                  if isDoesNotExistError e
+                    then
+                      die
+                        "Error: Could not find token file, generate one with \"--auth\" argument"
+                    else die $ "Error opening token file: \n" <> show e
               )
 
   case cfgFile of
-    Left  e -> die $ "Error parsing token file \"" <> tp <> "\":\n" <> e
+    Left e -> die $ "Error parsing token file \"" <> tp <> "\":\n" <> e
     Right c -> return c
 
 newTokenFile :: AuthCode -> IO ()
@@ -141,9 +158,10 @@ newTokenFile ac = do
   exists <- doesFileExist =<< tokenPath
   if exists
     then tokenPath >>= die . ("Token file already exists at: " <>)
-    else getAccessToken ac CS.clientSecret >>= \case
-      Left  e -> die $ "Error getting token file:\n" <> show e
-      Right t -> do
-        tp <- tokenPath
-        encodeFile tp t
-        putStrLn $ "Saved tokenfile to:\n" <> tp
+    else
+      getAccessToken ac CS.clientSecret >>= \case
+        Left e -> die $ "Error getting token file:\n" <> show e
+        Right t -> do
+          tp <- tokenPath
+          encodeFile tp t
+          putStrLn $ "Saved tokenfile to:\n" <> tp
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,97 +1,111 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Lib
-  ( FnToken(..)
-  , CustomerInvoice(..)
-  , SupplierInvoice(..)
-  , FnError(..)
-  , getInvoices
-  , getAccessToken
-  , ClientSecret
-  , AuthCode
-  , AccessToken
+  ( FnToken (..),
+    CustomerInvoice (..),
+    SupplierInvoice (..),
+    FnError (..),
+    getInvoices,
+    getAccessToken,
+    ClientSecret,
+    AuthCode,
+    AccessToken,
   )
 where
 
-import           Control.Exception
-import           Control.Concurrent.Async       ( concurrently )
-import           Network.HTTP.Client
-import           Network.HTTP.Simple
-import           Network.HTTP.Types
-import           Data.Aeson
-import           Data.Aeson.Types
-import           Data.Bifunctor                 ( first )
-import           GHC.Generics
-import           Data.Time.Calendar             ( Day )
-import qualified Data.ByteString.Char8         as S8
-
+import Control.Concurrent.Async (concurrently)
+import Control.Exception
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Bifunctor (first)
+import qualified Data.ByteString.Char8 as S8
+import Data.Time.Calendar (Day)
+import GHC.Generics
+import Network.HTTP.Client
+import Network.HTTP.Simple
+import Network.HTTP.Types
 
 -- | An invoice to a customer
 data CustomerInvoice = CustomerInvoice
-  { cNumber :: String   -- ^ The Reference number
-  , cBalance :: Float   -- ^ Balance of the invoice in cCurrency
-  , cName :: String     -- ^ Name of the customer
-  , cDueDate :: Day     -- ^ Due Date
-  , cCurrency :: String -- ^  Currency of the Invoice
-  } deriving (Eq, Show)
+  { -- | The Reference number
+    cNumber :: String,
+    -- | Balance of the invoice in cCurrency
+    cBalance :: Float,
+    -- | Name of the customer
+    cName :: String,
+    -- | Due Date
+    cDueDate :: Day,
+    -- |  Currency of the Invoice
+    cCurrency :: String
+  }
+  deriving (Eq, Show)
 
 instance FromJSON CustomerInvoice where
   parseJSON (Object o) =
     CustomerInvoice <$> o .: "DocumentNumber"
-    <*> o .: "Balance"
-    <*> o .: "CustomerName"
-    <*> o .: "DueDate"
-    <*> o .: "Currency"
-  parseJSON invalid    = typeMismatch "Customer Invoice" invalid
+      <*> o .: "Balance"
+      <*> o .: "CustomerName"
+      <*> o .: "DueDate"
+      <*> o .: "Currency"
+  parseJSON invalid = typeMismatch "Customer Invoice" invalid
 
 -- | A wrapper of lists of invoices used for parsing
-newtype CustomerInvoices =  CustomerInvoices { getCInv ::[CustomerInvoice] } deriving (Show)
+newtype CustomerInvoices = CustomerInvoices {getCInv :: [CustomerInvoice]} deriving (Show)
 
 instance FromJSON CustomerInvoices where
   parseJSON (Object o) = CustomerInvoices <$> o .:? "Invoices" .!= []
-  parseJSON invalid    = typeMismatch "List of customer invoices" invalid
+  parseJSON invalid = typeMismatch "List of customer invoices" invalid
 
 -- | An Invoice from a supplier
 data SupplierInvoice = SupplierInvoice
-  { sNumber :: String    -- ^ The Reference number
-  , sTotal :: Float      -- ^ Total of the invoice in sCurrency
-  , sName :: String      -- ^ Name of the Supplier
-  , sDueDate :: Day      -- ^ Due Date
-  , sCurrency :: String  -- ^ Currency of the Invoice
-  } deriving (Eq, Show)
+  { -- | The Reference number
+    sNumber :: String,
+    -- | Total of the invoice in sCurrency
+    sTotal :: Float,
+    -- | Name of the Supplier
+    sName :: String,
+    -- | Due Date
+    sDueDate :: Day,
+    -- | Currency of the Invoice
+    sCurrency :: String
+  }
+  deriving (Eq, Show)
 
 instance FromJSON SupplierInvoice where
   parseJSON (Object o) =
     SupplierInvoice <$> o .: "GivenNumber"
-    <*> (read <$> (o .: "Total"))
-    <*> o .: "SupplierName"
-    <*> o .: "DueDate"
-    <*> o .: "Currency"
-  parseJSON invalid    = typeMismatch "Supplier Invoice" invalid
+      <*> (read <$> (o .: "Total"))
+      <*> o .: "SupplierName"
+      <*> o .: "DueDate"
+      <*> o .: "Currency"
+  parseJSON invalid = typeMismatch "Supplier Invoice" invalid
 
 -- | A wrapper of lists of invoices used for parsing
-newtype SupplierInvoices =  SupplierInvoices { getSInv :: [SupplierInvoice] }deriving (Show)
+newtype SupplierInvoices = SupplierInvoices {getSInv :: [SupplierInvoice]} deriving (Show)
 
 instance FromJSON SupplierInvoices where
   parseJSON (Object o) = SupplierInvoices <$> o .:? "SupplierInvoices" .!= []
-  parseJSON invalid    = typeMismatch "List of supplier invoices" invalid
+  parseJSON invalid = typeMismatch "List of supplier invoices" invalid
 
 -- | Get all unpaid invoices
-getInvoices
-  :: FnToken -> IO (Either FnError ([CustomerInvoice], [SupplierInvoice]))
+getInvoices ::
+  FnToken -> IO (Either FnError ([CustomerInvoice], [SupplierInvoice]))
 getInvoices token = do
-  (cResp, sResp) <- concurrently (sendRequest clientInvReq)
-                                 (sendRequest suppliInvReq)
+  (cResp, sResp) <-
+    concurrently
+      (sendRequest clientInvReq)
+      (sendRequest suppliInvReq)
   return $ (,) <$> (getCInv <$> cResp) <*> (getSInv <$> sResp)
- where
-  clientInvReq =
-    addAccessToken token . setRequestPath "/3/invoices/?filter=unpaidoverdue"
-  suppliInvReq = addAccessToken token
-    . setRequestPath "/3/supplierinvoices/?filter=unpaidoverdue"
+  where
+    clientInvReq =
+      addAccessToken token . setRequestPath "/3/invoices/?filter=unpaidoverdue"
+    suppliInvReq =
+      addAccessToken token
+        . setRequestPath "/3/supplierinvoices/?filter=unpaidoverdue"
 
 addAccessToken :: FnToken -> (Request -> Request)
 addAccessToken token =
@@ -102,21 +116,21 @@ sendRequest :: FromJSON a => (Request -> Request) -> IO (Either FnError a)
 sendRequest reqLense = do
   eresp <- try $ httpJSONEither (reqLense baseReq') -- `catches` fnErrHandler
   case eresp of
-    Left  e    -> return $ Left $ ConnectFail e
+    Left e -> return $ Left $ ConnectFail e
     Right resp -> do
       let respStatus = getResponseStatus resp
       return $ case statusCode respStatus of
         200 -> first ParseFail $ getResponseBody resp
-        _   -> Left $ HttpError $ S8.unpack $ statusMessage respStatus
- where
-  baseReq' =
-    setRequestMethod "GET"
-      $ setRequestHeaders headers
-      $ setRequestSecure True
-      $ setRequestPort 443
-      $ parseRequest_ "http://api.fortnox.se"
-  headers =
-    [("Accept", "application/json"), ("Content-type", "application/json")]
+        _ -> Left $ HttpError $ S8.unpack $ statusMessage respStatus
+  where
+    baseReq' =
+      setRequestMethod "GET" $
+        setRequestHeaders headers $
+          setRequestSecure True $
+            setRequestPort 443 $
+              parseRequest_ "http://api.fortnox.se"
+    headers =
+      [("Accept", "application/json"), ("Content-type", "application/json")]
 
 -- | Secret accociated with the app
 type ClientSecret = String
@@ -128,27 +142,30 @@ type AuthCode = String
 type AccessToken = String
 
 -- | FnToken contains whats needed to talk to the API
-data FnToken = FnToken { accessToken :: AccessToken
-                       , clientSecret :: ClientSecret }
-                       deriving (Generic, Eq,Show)
+data FnToken = FnToken
+  { accessToken :: AccessToken,
+    clientSecret :: ClientSecret
+  }
+  deriving (Generic, Eq, Show)
 
 instance FromJSON FnToken
+
 instance ToJSON FnToken
 
-newtype AuthReply = AuthReply { getAuth :: AccessToken } deriving Show
+newtype AuthReply = AuthReply {getAuth :: AccessToken} deriving (Show)
 
 instance FromJSON AuthReply where
-  parseJSON (Object o) = AuthReply <$> (o.: "Authorization" >>=  (.: "AccessToken"))
-  parseJSON invalid    = typeMismatch "Authorization reply" invalid
+  parseJSON (Object o) = AuthReply <$> (o .: "Authorization" >>= (.: "AccessToken"))
+  parseJSON invalid = typeMismatch "Authorization reply" invalid
 
 -- | Generate a new access token, can only be run once for each AuthCode
 getAccessToken :: AuthCode -> ClientSecret -> IO (Either FnError FnToken)
 getAccessToken authCode secret = do
   resp <-
-    sendRequest
-    $ setRequestPath "/3/invoices"
-    . addRequestHeader "Authorization-Code" (S8.pack authCode)
-    . addRequestHeader "Client-Secret"      (S8.pack secret)
+    sendRequest $
+      setRequestPath "/3/invoices"
+        . addRequestHeader "Authorization-Code" (S8.pack authCode)
+        . addRequestHeader "Client-Secret" (S8.pack secret)
   let token = flip FnToken secret . getAuth <$> resp
   return token
 
